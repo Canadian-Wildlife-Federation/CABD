f016a91b110f85f7defce733e5fe963ca8afbec9
Emily Gouge <egouge@refractions.net>
2022-01-19 14:31:22 -0800

    cors support

--- cabd-web/src/main/java/org/refractions/cabd/WebConfig.java
+++ cabd-web/src/main/java/org/refractions/cabd/WebConfig.java
@@ -18,6 +18,7 @@ package org.refractions.cabd;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -38,5 +39,12 @@ public class WebConfig implements WebMvcConfigurer {
 				.mediaType("kml", CabdApplication.KML_MEDIA_TYPE)
 			;
 	}
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET","POST","PUT","DELETE");
+    }
 	
 }
