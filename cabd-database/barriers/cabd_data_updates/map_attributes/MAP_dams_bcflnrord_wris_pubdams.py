468f97e7ffbfc984b1fdce9a1a80c8821881888c
Katherine O'Hearn <KatherineO@cwf-fcf.org>
2022-03-18 11:05:32 -0400

    Update bcflnrord_wris_pubdams mapping script to only populate _ds fields

--- cabd-database/barriers/cabd_data_updates/map_attributes/MAP_dams_bcflnrord_wris_pubdams.py
+++ cabd-database/barriers/cabd_data_updates/map_attributes/MAP_dams_bcflnrord_wris_pubdams.py
@@ -1,6 +1,6 @@
 import MAP_attributes_main as main
 
-script = main.MappingScript("wrispublicdams")
+script = main.MappingScript("bcflnrord_wris_pubdams")
     
 mappingquery = f"""
 
@@ -29,23 +29,13 @@ SET
     construction_year_ds = CASE WHEN (cabd.construction_year IS NULL AND {script.datasetname}.construction_year IS NOT NULL) THEN {script.datasetname}.data_source ELSE cabdsource.construction_year_ds END,
     height_m_ds = CASE WHEN (cabd.height_m IS NULL AND {script.datasetname}.height_m IS NOT NULL) THEN {script.datasetname}.data_source ELSE cabdsource.height_m_ds END,
     length_m_ds = CASE WHEN (cabd.length_m IS NULL AND {script.datasetname}.length_m IS NOT NULL) THEN {script.datasetname}.data_source ELSE cabdsource.length_m_ds END,
-    operating_status_code_ds = CASE WHEN (cabd.operating_status_code IS NULL AND {script.datasetname}.operating_status_code IS NOT NULL) THEN {script.datasetname}.data_source ELSE cabdsource.operating_status_code_ds END,    
-    
-    dam_name_en_dsfid = CASE WHEN (cabd.dam_name_en IS NULL AND {script.datasetname}.dam_name_en IS NOT NULL) THEN {script.datasetname}.data_source_id ELSE cabdsource.dam_name_en_dsfid END,
-    owner_dsfid = CASE WHEN (cabd.owner IS NULL AND {script.datasetname}.owner IS NOT NULL) THEN {script.datasetname}.data_source_id ELSE cabdsource.owner_dsfid END,
-    ownership_type_code_dsfid = CASE WHEN (cabd.ownership_type_code IS NULL AND {script.datasetname}.ownership_type_code IS NOT NULL) THEN {script.datasetname}.data_source_id ELSE cabdsource.ownership_type_code_dsfid END,
-    construction_type_code_dsfid = CASE WHEN (cabd.construction_type_code IS NULL AND {script.datasetname}.construction_type_code IS NOT NULL) THEN {script.datasetname}.data_source_id ELSE cabdsource.construction_type_code_dsfid END,
-    function_code_dsfid = CASE WHEN (cabd.function_code IS NULL AND {script.datasetname}.function_code IS NOT NULL) THEN {script.datasetname}.data_source_id ELSE cabdsource.function_code_dsfid END,
-    construction_year_dsfid = CASE WHEN (cabd.construction_year IS NULL AND {script.datasetname}.construction_year IS NOT NULL) THEN {script.datasetname}.data_source_id ELSE cabdsource.construction_year_dsfid END,
-    height_m_dsfid = CASE WHEN (cabd.height_m IS NULL AND {script.datasetname}.height_m IS NOT NULL) THEN {script.datasetname}.data_source_id ELSE cabdsource.height_m_dsfid END,
-    length_m_dsfid = CASE WHEN (cabd.length_m IS NULL AND {script.datasetname}.length_m IS NOT NULL) THEN {script.datasetname}.data_source_id ELSE cabdsource.length_m_dsfid END,
-    operating_status_code_dsfid = CASE WHEN (cabd.operating_status_code IS NULL AND {script.datasetname}.operating_status_code IS NOT NULL) THEN {script.datasetname}.data_source_id ELSE cabdsource.operating_status_code_dsfid END    
+    operating_status_code_ds = CASE WHEN (cabd.operating_status_code IS NULL AND {script.datasetname}.operating_status_code IS NOT NULL) THEN {script.datasetname}.data_source ELSE cabdsource.operating_status_code_ds END    
 FROM
     {script.damTable} AS cabd,
     {script.workingTable} AS {script.datasetname}    
 WHERE
     cabdsource.cabd_id = {script.datasetname}.cabd_id and cabd.cabd_id = cabdsource.cabd_id;
-    
+  
 --update existing features 
 UPDATE
     {script.damTable} AS cabd
