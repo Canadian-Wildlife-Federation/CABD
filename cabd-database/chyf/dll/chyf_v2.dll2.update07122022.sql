0b5fdb06d834a376dd44efad37f8eca2a99338e1
Katherine O'Hearn <KatherineO@cwf-fcf.org>
2022-07-22 10:55:55 -0400

    Update queries to respect lowercase names for processing schemas

--- cabd-database/chyf/dll/chyf_v2.dll2.update07122022.sql
+++ cabd-database/chyf/dll/chyf_v2.dll2.update07122022.sql
@@ -14,15 +14,15 @@ alter table chyf2.ecatchment add column nid varchar(32);
 --to update processed layers in the chyf2 schema run this query then run
 -- the queries that this query outputs
 select 
-'with edges as (select c.id, a.nid from nhn' || short_name || '.eflowpath a join fpoutput.eflowpath b on a.id = b.id join chyf2.eflowpath c on b.internal_id  = c.id) update chyf2.eflowpath set nid = ed.nid from edges ed where ed.id = chyf2.eflowpath.id'
+'with edges as (select c.id, a.nid from nhn' || LOWER(short_name) || '.eflowpath a join fpoutput.eflowpath b on a.id = b.id join chyf2.eflowpath c on b.internal_id  = c.id) update chyf2.eflowpath set nid = ed.nid from edges ed where ed.id = chyf2.eflowpath.id'
 from chyf2.aoi;
 
 --do the same for ecatchments
 select id, short_name, 
-'with wbs as (select c.id, a.nid from nhn' || short_name || '.ecatchment a join fpoutput.ecatchment b on a.id = b.id join chyf2.ecatchment c on b.internal_id  = c.id) update chyf2.ecatchment set nid = w.nid from wbs w where w.id = chyf2.ecatchment.id'
+'with wbs as (select c.id, a.nid from nhn' || LOWER(short_name) || '.ecatchment a join fpoutput.ecatchment b on a.id = b.id join chyf2.ecatchment c on b.internal_id  = c.id) update chyf2.ecatchment set nid = w.nid from wbs w where w.id = chyf2.ecatchment.id'
 from chyf2.aoi
 
---to check that most nids are copies correctly:
+--to check that most nids are copied correctly:
 -- for flowpaths most nids should be not null, but all skeletons will be null
 -- for a single aoi if all nids are null then the data did not get copied correctly
 select a.short_name, case when b.nid is null then 'NULL' else 'NOT NULL' end as has_nid, count(*)
@@ -42,15 +42,15 @@ order by a.short_name;
 --now if you want to update aois in fpoutput that have not
 --yet been loaded into chyf2 schema
 select 
-'with edges as (select b.id, a.nid from nhn' || name || '.eflowpath a join fpoutput.eflowpath b on a.id = b.id ) update fpoutput.eflowpath set nid = ed.nid from edges ed where ed.id = fpoutput.eflowpath.id'
+'with edges as (select b.id, a.nid from nhn' || LOWER(name) || '.eflowpath a join fpoutput.eflowpath b on a.id = b.id ) update fpoutput.eflowpath set nid = ed.nid from edges ed where ed.id = fpoutput.eflowpath.id'
 from fpoutput.aoi where status != 'CHYF_DONE'
 
 --same for ecatchments
 select 
-'with wbs as (select b.id, a.nid from nhn' || name || '.ecatchment a join fpoutput.ecatchment b on a.id = b.id ) update fpoutput.ecatchment set nid = w.nid from wbs w where w.id = fpoutput.ecatchment.id'
+'with wbs as (select b.id, a.nid from nhn' || LOWER(name) || '.ecatchment a join fpoutput.ecatchment b on a.id = b.id ) update fpoutput.ecatchment set nid = w.nid from wbs w where w.id = fpoutput.ecatchment.id'
 from fpoutput.aoi where status != 'CHYF_DONE'
 
---to check that most nids are copies correctly:
+--to check that most nids are copied correctly:
 -- for flowpaths most nids should be not null, but all skeletons will be null
 -- for a single aoi if all nids are null then the data did not get copied correctly
 select a.name, case when b.nid is null then 'NULL' else 'NOT NULL' end as has_nid, count(*)
