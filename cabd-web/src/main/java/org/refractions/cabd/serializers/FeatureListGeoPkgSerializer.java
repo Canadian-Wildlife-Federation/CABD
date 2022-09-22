ca5292f3fba94ca9676b42fffee04f62af1f7fb5
Emily Gouge <egouge@refractions.net>
2022-09-22 13:25:43 -0700

    Revert "Update Export Filenames"

--- cabd-web/src/main/java/org/refractions/cabd/serializers/FeatureListGeoPkgSerializer.java
+++ cabd-web/src/main/java/org/refractions/cabd/serializers/FeatureListGeoPkgSerializer.java
@@ -55,6 +55,8 @@ public class FeatureListGeoPkgSerializer extends AbstractFeatureListSerializer{
 	private FeatureTypeManager typeManager;
 	
 	private Logger logger = LoggerFactory.getLogger(FeatureListGeoPkgSerializer.class);
+
+	private static final String FILENAME = "features";
 	
 	public FeatureListGeoPkgSerializer() {
 		super(CabdApplication.GEOPKG_MEDIA_TYPE);
@@ -92,7 +94,7 @@ public class FeatureListGeoPkgSerializer extends AbstractFeatureListSerializer{
 				tx.commit();
 			}
 			
-			outputMessage.getHeaders().set(HttpHeaders.CONTENT_DISPOSITION, FeatureListUtil.getContentDispositionHeader(metadataitems.getLeft(), "gpkg"));
+			outputMessage.getHeaders().set(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + FILENAME + ".gpkg");
 			outputMessage.getHeaders().set(HttpHeaders.CONTENT_TYPE, CabdApplication.GEOPKG_MEDIA_TYPE_STR);
 	
 			Files.copy(temp, outputMessage.getBody());
