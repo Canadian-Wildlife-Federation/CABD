4344380bb8d709c0b2aba8b44eaf000ef6dc9888
Katherine O'Hearn <KatherineO@cwf-fcf.org>
2023-01-31 11:29:18 -0500

    Update coded_value_table_changes.sql
    - Null out coded values and change to new values where appropriate, maintaining original codes for checks later on

--- cabd-database/barriers/data_structure/coded_value_table_changes.sql
+++ cabd-database/barriers/data_structure/coded_value_table_changes.sql
@@ -11,7 +11,7 @@
 ALTER TABLE dams.dams DISABLE TRIGGER ALL;
 ALTER TABLE dams.structure_type_codes DISABLE TRIGGER ALL;
 ALTER TABLE dams.function_codes DISABLE TRIGGER ALL;
-ALTER TABLE dams.size_class_codes DISABLE TRIGGER ALL;
+ALTER TABLE dams.size_codes DISABLE TRIGGER ALL;
 ALTER TABLE dams.dam_use_codes DISABLE TRIGGER ALL;
 ALTER TABLE dams.operating_status_codes DISABLE TRIGGER ALL;
 ALTER TABLE cabd.barrier_ownership_type_codes DISABLE TRIGGER ALL;
@@ -24,14 +24,14 @@ DELETE FROM dams.structure_type_codes;
 INSERT INTO dams.structure_type_codes
     (code, name_en, description_en, name_fr, description_fr)
 SELECT
-    code, name, description, name, description
+    code, name_en, description_en, name_fr, description_fr
 FROM dams.temp_structure_type_codes;
 
 DELETE FROM dams.function_codes;
 INSERT INTO dams.function_codes
     (code, name_en, description_en, name_fr, description_fr)
 SELECT
-    code, name, description, name, description
+    code, name_en, description_en, name_fr, description_fr
 FROM dams.temp_function_codes;
 
 --------------------------------------------
@@ -79,30 +79,34 @@ WHERE size_class_code = 4;
 -- dam use
 --------------------------------------------
 
+UPDATE dams.dams SET use_code = NULL WHERE use_code IN (10, 11, 12);
+
 UPDATE dams.dam_use_codes
 SET code = 99 WHERE name_en = 'Unknown';
 
 UPDATE dams.dams
 SET use_code = (SELECT code FROM dams.dam_use_codes WHERE name_en = 'Unknown') 
-WHERE use_code = 11;
-
-UPDATE dams.dam_use_codes
-SET code = 10 WHERE name_en = 'Wildlife Conservation';
-
-UPDATE dams.dams
-SET use_code = (SELECT code FROM dams.dam_use_codes WHERE name_en = 'Wildlife Conservation')
-WHERE use_code = 12;
+WHERE dam_use_code_old = 11;
 
 UPDATE dams.dam_use_codes
 SET code = 11 WHERE name_en = 'Other';
 
 UPDATE dams.dams
 SET use_code = (SELECT code FROM dams.dam_use_codes WHERE name_en = 'Other')
-WHERE use_code = 10;
+WHERE dam_use_code_old = 10;
+
+UPDATE dams.dam_use_codes
+SET code = 10 WHERE name_en = 'Wildlife Conservation';
+
+UPDATE dams.dams
+SET use_code = (SELECT code FROM dams.dam_use_codes WHERE name_en = 'Wildlife Conservation')
+WHERE dam_use_code_old = 12;
 
 --------------------------------------------
 -- operating status
 --------------------------------------------
+
+UPDATE dams.dams SET operating_status_code = NULL WHERE operating_status_code IN (5,6);
 
 UPDATE dams.operating_status_codes
 SET code = 99 WHERE name_en = 'Unknown';
@@ -117,18 +121,20 @@ SET name_en =
 
 UPDATE dams.dams
 SET operating_status_code = (SELECT code FROM dams.operating_status_codes WHERE name_en = 'Unknown') 
-WHERE operating_status_code = 5;
+WHERE operating_status_code_old = 5;
 
 UPDATE dams.operating_status_codes
 SET code = 5 WHERE name_en = 'Remediated';
 
 UPDATE dams.dams
 SET operating_status_code = (SELECT code FROM dams.operating_status_codes WHERE name_en = 'Remediated') 
-WHERE operating_status_code = 6;
+WHERE operating_status_code_old = 6;
 
 --------------------------------------------
 -- ownership type
 --------------------------------------------
+
+UPDATE dams.dams SET ownership_type_code = NULL WHERE ownership_type_code IN (7,8);
 
 UPDATE cabd.barrier_ownership_type_codes
 SET code = 99 WHERE name_en = 'Unknown';
@@ -142,14 +148,14 @@ SET name_en =
 
 UPDATE dams.dams
 SET ownership_type_code = (SELECT code FROM cabd.barrier_ownership_type_codes WHERE name_en = 'Unknown') 
-WHERE ownership_type_code = 7;
+WHERE ownership_type_code_old = 7;
 
 UPDATE cabd.barrier_ownership_type_codes
 SET code = 7 WHERE name_en = 'Indigenous';
 
 UPDATE dams.dams
 SET ownership_type_code = (SELECT code FROM cabd.barrier_ownership_type_codes WHERE name_en = 'Indigenous') 
-WHERE ownership_type_code = 8;
+WHERE ownership_type_code_old = 8;
 
 --------------------------------------------
 -- spillway type
@@ -166,19 +172,21 @@ WHERE spillway_type_code = 6;
 -- turbine type
 --------------------------------------------
 
+UPDATE dams.dams SET turbine_type_code = NULL WHERE turbine_type_code IN (5,6);
+
 UPDATE dams.turbine_type_codes
 SET code = 99 WHERE name_en = 'Unknown';
 
 UPDATE dams.dams
 SET turbine_type_code = (SELECT code FROM dams.turbine_type_codes WHERE name_en = 'Unknown') 
-WHERE turbine_type_code = 5;
+WHERE turbine_type_code_old = 5;
 
 UPDATE dams.turbine_type_codes
 SET code = 5 WHERE name_en = 'Other';
 
 UPDATE dams.dams
 SET turbine_type_code = (SELECT code FROM dams.turbine_type_codes WHERE name_en = 'Other') 
-WHERE turbine_type_code = 6;
+WHERE turbine_type_code_old = 6;
 
 --------------------------------------------
 -- upstream passage type
@@ -196,14 +204,60 @@ SET fishpass_type_code = (SELECT code FROM cabd.upstream_passage_type_codes WHER
 WHERE fishpass_type_code = 9;
 
 --------------------------------------------
--- structure type - just for ones where
--- original data source no longer accessible
--- or this info is no longer included
+-- function
 --------------------------------------------
+
+UPDATE dams.dams SET function_code = NULL WHERE function_code > 3;
+
+UPDATE dams.dams
+SET function_code = (SELECT code FROM dams.function_codes WHERE name_en = 'Unknown')
+WHERE function_code_old IN (4,5);
+
+UPDATE dams.dams
+SET function_code = (SELECT code FROM dams.function_codes WHERE name_en = 'Saddle')
+WHERE function_code_old = 6;
+
+UPDATE dams.dams
+SET function_code = (SELECT code FROM dams.function_codes WHERE name_en = 'Hydro - Closed-cycle pumped storage')
+WHERE function_code_old = 7;
+
+UPDATE dams.dams
+SET function_code = (SELECT code FROM dams.function_codes WHERE name_en = 'Hydro - Conventional storage')
+WHERE function_code_old = 8;
+
+UPDATE dams.dams
+SET function_code = (SELECT code FROM dams.function_codes WHERE name_en = 'Hydro - Open-cycle pumped storage')
+WHERE function_code_old = 9;
+
+UPDATE dams.dams
+SET function_code = (SELECT code FROM dams.function_codes WHERE name_en = 'Hydro - Run-of-river')
+WHERE function_code_old = 10;
+
+UPDATE dams.dams
+SET function_code = (SELECT code FROM dams.function_codes WHERE name_en = 'Hydro - Tidal')
+WHERE function_code_old = 11;
+
+UPDATE dams.dams
+SET function_code = (SELECT code FROM dams.function_codes WHERE name_en = 'Other')
+WHERE function_code_old = 12;
+
+UPDATE dams.dams
+SET function_code = (SELECT code FROM dams.function_codes WHERE name_en = 'Unknown')
+WHERE function_code_old = 13;
+
+--------------------------------------------
+-- structure type
+--------------------------------------------
+
+UPDATE dams.dams SET structure_type_code = NULL WHERE structure_type_code > 5;
+
+UPDATE dams.dams
+SET structure_type_code = (SELECT code FROM dams.structure_type_codes WHERE name_en = 'Unknown')
+WHERE structure_type_code_old = 9;
 
 UPDATE dams.dams
 SET structure_type_code = (SELECT code FROM dams.structure_type_codes WHERE name_en = 'Other')
-WHERE structure_type_code = 10;
+WHERE structure_type_code_old = 10;
 
 --------------------------------------------
 -- re-enable triggers
@@ -212,7 +266,7 @@ WHERE structure_type_code = 10;
 ALTER TABLE dams.dams ENABLE TRIGGER ALL;
 ALTER TABLE dams.structure_type_codes ENABLE TRIGGER ALL;
 ALTER TABLE dams.function_codes ENABLE TRIGGER ALL;
-ALTER TABLE dams.size_class_codes ENABLE TRIGGER ALL;
+ALTER TABLE dams.size_codes ENABLE TRIGGER ALL;
 ALTER TABLE dams.dam_use_codes ENABLE TRIGGER ALL;
 ALTER TABLE dams.operating_status_codes ENABLE TRIGGER ALL;
 ALTER TABLE cabd.barrier_ownership_type_codes ENABLE TRIGGER ALL;
