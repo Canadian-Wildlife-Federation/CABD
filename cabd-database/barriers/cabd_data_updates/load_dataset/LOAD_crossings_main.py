39e3cd32e62ca3d238faa11c3f97f4076cac4b29
Katherine O'Hearn <KatherineO@cwf-fcf.org>
2023-03-16 10:05:37 -0400

    Create LOAD_crossings_main.py

---
+++ cabd-database/barriers/cabd_data_updates/load_dataset/LOAD_crossings_main.py
@@ -0,0 +1,93 @@
+import psycopg2 as pg2
+import subprocess
+import sys
+
+ogr = "C:\\OSGeo4W64\\bin\\ogr2ogr.exe"
+
+dbHost = "cabd-postgres.postgres.database.azure.com"
+dbPort = "5432"
+dbName = "cabd"
+dbUser = sys.argv[2]
+dbPassword = sys.argv[3]
+
+class LoadingScript:
+
+    #create source data table and tables to be mapped into CABD dataset
+    sourceSchema = "source_data"
+    workingSchema = "featurecopy"
+    
+    sourceTable = ""
+
+    nonTidalSites = ""
+    nonTidalStructures = ""
+
+    tidalSites = ""
+    tidalStructures = ""
+
+    materialMapping = "material_mapping"
+
+    physicalBarrierMapping = "physical_barrier_mapping"
+    
+    datasetname = ""
+    datafile = ""
+    
+    def __init__(self, datasetname):
+        self.datasetname = datasetname
+        self.sourceTable = self.sourceSchema + "." + datasetname
+
+        self.nonTidalSites = self.workingSchema + ".nontidal_sites_" + datasetname
+        self.nonTidalStructures = self.workingSchema + ".nontidal_structures_" + datasetname
+
+        self.tidalSites = self.workingSchema + ".tidal_sites_" + datasetname
+        self.tidalStructures = self.workingSchema + ".tidal_structures_" + datasetname        
+
+        self.materialMappingTable = self.workingSchema + "." + self.materialMapping
+        self.physicalBarrierMappingTable = self.workingSchema + "." + self.physicalBarrierMapping
+        
+        self.datafile = sys.argv[1]
+        
+        if len(sys.argv) != 4:
+            print('Invalid usage: py LOAD_<datasetname>.py "<datafile>" <dbUser> <dbPassword>')
+            sys.exit()
+
+    def do_work(self, loadquery):
+        print("Loading data from file " +  self.datafile)
+
+        self.conn = pg2.connect(database=dbName, 
+                   user=dbUser, 
+                   host=dbHost, 
+                   password=dbPassword, 
+                   port=dbPort)
+
+        self.initdb()
+        self.load_data(self.datafile)
+        print("Splitting by site type and mapping attributes to data model...")
+        self.run_load_query(loadquery)
+        
+        self.conn.commit()
+        self.conn.close()
+        
+        print("Script complete")
+        print("Data loaded into schemas: " + self.sourceSchema + " (original data), " + self.workingSchema + " (mapped data)")
+
+    #load data from geopackage file into the database
+    def load_data(self, filename):
+        #load data using ogr
+        orgDb="dbname='" + dbName + "' host='"+ dbHost+"' port='"+dbPort+"' user='"+dbUser+"' password='"+ dbPassword+"'"
+        pycmd = '"' + ogr + '" -f "PostgreSQL" PG:"' + orgDb + '" -nln "' + self.sourceTable + '" -lco GEOMETRY_NAME=geometry -nlt PROMOTE_TO_MULTI ' + '"' + filename + '"'
+        print(pycmd)
+        subprocess.run(pycmd)
+
+    def run_load_query(self, loadquery):
+        with self.conn.cursor() as cursor:
+            cursor.execute(loadquery)
+
+    def initdb(self):
+        query = f"""
+        CREATE SCHEMA IF NOT EXISTS {self.sourceSchema};
+        CREATE SCHEMA IF NOT EXISTS {self.workingSchema};
+        DROP TABLE IF EXISTS {self.sourceTable};
+        """
+        with self.conn.cursor() as cursor:
+            cursor.execute(query)
+        self.conn.commit()
