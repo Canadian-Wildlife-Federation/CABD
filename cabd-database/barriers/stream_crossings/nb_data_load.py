0ffee770dd7dda1a8feaf4fa1e632dd3cd49f20c
Katherine O'Hearn <katherineo@cwf-fcf.org>
2024-01-24 13:27:38 -0500

    Update NB scripts to be compatible with new format

--- cabd-database/barriers/stream_crossings/nb_data_load.py
+++ cabd-database/barriers/stream_crossings/nb_data_load.py
@@ -1,69 +1,73 @@
-import psycopg2 as pg2
 import subprocess
-import sys
 import os
+import argparse
+import configparser
+import getpass
+import psycopg2 as pg2
+
+parser = argparse.ArgumentParser(description='Processing stream crossings.')
+parser.add_argument('-c', type=str, help='the configuration file', required=True)
+parser.add_argument('-file', type=str, help='the file containing data to load')
+args = parser.parse_args()
+configfile = args.c
+
+#-- READ PARAMETERS FOR CONFIG FILE --
+config = configparser.ConfigParser()
+config.read(configfile)
+
+#database settings 
+dbHost = config['DATABASE']['host']
+dbPort = config['DATABASE']['port']
+dbName = config['DATABASE']['name']
+dbUser = input(f"""Enter username to access {dbName}:\n""")
+dbPassword = getpass.getpass(f"""Enter password to access {dbName}:\n""")
+srcFile = args.file
+
+#output data schema
+schema = config['DATABASE']['data_schema']
+cabdSRID = config['DATABASE']['cabdSRID']
+mSRID  = config['SETTINGS']['mSRID']
 
 def log(message):
-    if (1):
+    if 1:
         print(message)
-        
-        
+
+my_env = os.environ.copy()
+
 #alternate ogr options
-ogr = "C:\\Program Files\\GDAL\\ogr2ogr.exe"
-#ogr = "C:\\Program Files\\QGIS 3.22.3\\bin\\ogr2ogr.exe"
+# ogr = "C:\\Program Files\\GDAL\\ogr2ogr.exe"
+print(os.environ.get('OGR'))
+ogr = os.environ.get('OGR')
 
-# os.environ["PROJ_LIB"] = "C:\Program Files\GDAL\projlib"
-# print(os.environ["PROJ_LIB"])
-
-dbHost = "cabd-postgres.postgres.database.azure.com"
-dbPort = "5432"
-dbName = "cabd"
-dbUser = sys.argv[1]
-dbPassword = sys.argv[2]
-
-srcfile = ""
-srcfile = sys.argv[3]
-#temporary schema for loading and manipulating data
-workingSchema = "nb_data_test"
-
-conn = pg2.connect(database=dbName, 
-                   user=dbUser, 
-                   host=dbHost, 
-                   password=dbPassword, 
+conn = pg2.connect(database=dbName,
+                   user=dbUser,
+                   host=dbHost,
+                   password=dbPassword,
                    port=dbPort)
 
 #create schema
 query = f"""
-CREATE SCHEMA IF NOT EXISTS {workingSchema};
+CREATE SCHEMA IF NOT EXISTS {schema};
 """
 with conn.cursor() as cursor:
     cursor.execute(query)
 conn.commit()
 
-log("loading data into: " + workingSchema)
-
+log("Loading data into: " + schema)
 
 #load files into db
 orgDb="dbname='" + dbName + "' host='"+ dbHost+"' port='"+dbPort+"' user='"+dbUser+"' password='"+ dbPassword+"'"
 
-
-my_env = os.environ.copy()
-#my_env["PGCLIENTENCODING"] = "LATIN1"
-
-pycmd = '"' + ogr + '" -f "PostgreSQL" PG:"' + orgDb + '" -nln "' + workingSchema + '.canvec_rail" -lco GEOMETRY_NAME=geometry -nlt MULTILINESTRING "' + srcfile + '" "canvec_50k_nb_transport_track_segment_1" -t_srs EPSG:4617"' 
+pycmd = '"' + ogr + '" -f "PostgreSQL" PG:"' + orgDb + '" -nln "' + schema + '.canvec_track" -lco GEOMETRY_NAME=geometry -nlt MULTILINESTRING "' + srcFile + '" "canvec_track" -t_srs EPSG:' + cabdSRID + ' -overwrite"'
 log(pycmd)
 subprocess.run(pycmd)
 
-pycmd = '"' + ogr + '" -f "PostgreSQL" PG:"' + orgDb + '" -nln "' + workingSchema + '.gnb_roads" -lco GEOMETRY_NAME=geometry -nlt MULTILINESTRING "' + srcfile + '" "gnb_forestry_roads" -t_srs EPSG:4617"'
+pycmd = '"' + ogr + '" -f "PostgreSQL" PG:"' + orgDb + '" -nln "' + schema + '.nbrn" -lco GEOMETRY_NAME=geometry -nlt MULTILINESTRING "' + srcFile + '" "nbrn" -t_srs EPSG:' + cabdSRID + ' -overwrite"'
 log(pycmd)
 subprocess.run(pycmd)
 
-pycmd = '"' + ogr + '" -f "PostgreSQL" PG:"' + orgDb + '" -nln "' + workingSchema + '.nbrn_roads" -lco GEOMETRY_NAME=geometry -nlt MULTILINESTRING "' + srcfile + '" "nbrn_road_segment_entity" -t_srs EPSG:4617"'
+pycmd = '"' + ogr + '" -f "PostgreSQL" PG:"' + orgDb + '" -nln "' + schema + '.gnb_forestry_roads" -lco GEOMETRY_NAME=geometry -nlt MULTILINESTRING "' + srcFile + '" "gnb_forestry_roads" -t_srs EPSG:' + cabdSRID + ' -overwrite"'
 log(pycmd)
 subprocess.run(pycmd)
 
-pycmd = '"' + ogr + '" -f "PostgreSQL" PG:"' + orgDb + '" -nln "' + workingSchema + '.nrhn_streams" -lco GEOMETRY_NAME=geometry -nlt MULTILINESTRING "' + srcfile + '" "nbhn_0000_01_watercourse" -t_srs EPSG:4617"'
-log(pycmd)
-subprocess.run(pycmd)
-      
-print ("LOAD DONE")
+log("LOAD DONE")
