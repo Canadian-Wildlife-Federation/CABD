89de722a5e06f5001123538b995d12dae54aef38
Emily Gouge <egouge@refractions.net>
2024-02-21 14:38:10 -0800

    minor change to upgrade script

--- cabd-database/cabd/cabd.updates.02162024.attributeset.sql
+++ cabd-database/cabd/cabd.updates.02162024.attributeset.sql
@@ -1,5 +1,7 @@
 create table cabd.attribute_set(name varchar, ft_metadata_col varchar, primary key (name));
 
+alter table cabd.attribute_set owner to cabd;
+
 insert into cabd.attribute_set(name, ft_metadata_col) values 
 ('all', 'vw_all_order'),
 ('limited', 'vw_simple_order'),
@@ -7,10 +9,11 @@ insert into cabd.attribute_set(name, ft_metadata_col) values
 
 alter table cabd.feature_type_metadata add column vw_mobile_order integer;
 
-update cabd.feature_type_metadata set vw_mobile_order = 1 whree field_name = 'cabd_id';
-update cabd.feature_type_metadata set vw_mobile_order = 2 whree field_name = 'passability_status';
-update cabd.feature_type_metadata set vw_mobile_order = 3 whree field_name = 'feature_type';
+update cabd.feature_type_metadata set vw_mobile_order = 1 where field_name = 'cabd_id';
+update cabd.feature_type_metadata set vw_mobile_order = 2 where field_name = 'passability_status';
+update cabd.feature_type_metadata set vw_mobile_order = 3 where field_name = 'feature_type';
 
 
 -- should ensure only geometry columns don't show up in this query
 select * from cabd.feature_type_metadata where vw_all_order is null;
+
