70782a5ae06b29c73e468eab74615440c9157025
Emily Gouge <egouge@refractions.net>
2026-06-25 16:42:26 -0700

    feat: add passability status to community api #95 fix bug with stream crossing processing #99, improved error handling

--- cabd-database/cabd/2026.06.19/community_data_view.sql
+++ cabd-database/cabd/2026.06.19/community_data_view.sql
@@ -1,7 +1,12 @@
 create or replace view cabd.community_data_staging_view as 
-select a.id, a.cabd_id, a.user_id, a.uploaded_datetime, a.data, 'stream_crossings' as feature_type, case when b.status is null then 'NEW' else b.status::varchar end as status
-from stream_crossings.stream_crossings_community_staging a left join
+select a.id, a.cabd_id, a.user_id, a.uploaded_datetime, a.data, 
+'stream_crossings' as feature_type, 
+case when b.status is null then 'NEW' else b.status::varchar end as status,
+case when c.passability_status_code is null then 4 else c.passability_status_code end as passability_status_code
+from stream_crossings.stream_crossings_community_staging s left join
 stream_crossings.stream_crossings_community_holding b on a.id = b.id 
+left join stream_crossings.structures c on c.site_id = b.cabd_id and c.primary_structure
 union
-select a.id, a.cabd_id, a.user_id, a.uploaded_datetime, a.data, 'dams' as feature_type, 'NEW'::varchar as status
-from dams.dams_community_staging a 
+select a.id, a.cabd_id, a.user_id, a.uploaded_datetime, a.data, 'dams' as feature_type, 'NEW'::varchar as status,
+case when b.passability_status_code is null then 4 else b.passability_status_code end as passability_status_code
+from dams.dams_community_staging a left join dams.dams b on b.cabd_id = a.cabd_id
