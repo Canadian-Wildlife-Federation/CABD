1b1f566c22cdece3b5dfd0a7fce451b89bf933ab
Emily Gouge <egouge@refractions.net>
2026-06-25 16:44:58 -0700

    bug: add missing update

--- cabd-database/cabd/2026.06.19/community_data_view.sql
+++ cabd-database/cabd/2026.06.19/community_data_view.sql
@@ -3,7 +3,7 @@ select a.id, a.cabd_id, a.user_id, a.uploaded_datetime, a.data,
 'stream_crossings' as feature_type, 
 case when b.status is null then 'NEW' else b.status::varchar end as status,
 case when c.passability_status_code is null then 4 else c.passability_status_code end as passability_status_code
-from stream_crossings.stream_crossings_community_staging s left join
+from stream_crossings.stream_crossings_community_staging a left join
 stream_crossings.stream_crossings_community_holding b on a.id = b.id 
 left join stream_crossings.structures c on c.site_id = b.cabd_id and c.primary_structure
 union
